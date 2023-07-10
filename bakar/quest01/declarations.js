4216839c9a26a10b6db7b0e79d0b73a04daabbf2
bakseck <seck.bakar@ugb.edu.sn>
2023-07-10 10:15:24 +0000

    quest01...

---
+++ bakar/quest01/declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "My str contains `,\\,/,\",and'"
+const arr = [4, '2']
+const obj = {
+    str : "bakar",
+    num : 20,
+    bool : true,
+    undef : undefined
+}
+const nested = {
+    arr : [4, undefined, '2'],
+    obj : {
+        str : "bakar",
+        num : 20,
+        bool : true
+    }
+}
---
+++ bakar/quest01/primitives.js
@@ -0,0 +1,4 @@
+const str = "SECK"
+const num = 20
+const bool = true
+const undef = undefined
